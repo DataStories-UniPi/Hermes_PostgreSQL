3c9683bcc53cee97e5c9441de0f3bb3b11a213b4
Vasilis-Thanos Anagnostopoulos <anagnwstopoulos@hotmail.com>
2015-06-05 17:16:47 +0300

    Adding the doxygen files

--- Hermes/C Library/Hermes.c
+++ Hermes/C Library/Hermes.c
@@ -1,5 +1,14 @@
-/*
- * Authors: Marios Vodas (mvodas@gmail.com).
+/**
+ * @file 
+ * @author Marios Vodas (mvodas@gmail.com).
+ * @date 9 Sep 2012
+ * @brief File containing example of doxygen usage for quick reference.
+ *
+ * Here typically goes a more extensive explanation of what the header
+ * defines. Doxygens tags are words preceeded by either a backslash @\
+ * or by an at symbol @@.
+ * @see http://www.stack.nl/~dimitri/doxygen/docblocks.html
+ * @see http://www.stack.nl/~dimitri/doxygen/commands.html
  */
 
 #include "h/Hermes.h"
---
+++ README.md
@@ -0,0 +1,9 @@
+# Hermes Moving Object Database
+
+Moving Object Database (MOD) engines enable us to process, manage and analyze mobility data. Hermes provides MOD functionality to OpenGIS-compatible state-of-the-art Object-Relational DBMS. In general, the big picture of Hermes aiming for the efficient management and exploration of mobility data is illustrated in the following figure.
+
+![Caption text](hermes_figure1.png)
+
+
+
+

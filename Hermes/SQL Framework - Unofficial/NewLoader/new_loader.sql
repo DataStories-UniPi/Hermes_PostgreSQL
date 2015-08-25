0728275571a0bf17188f5f2ca608fd53a6cdadab
kiriakosv <kir.velissariou@gmail.com>
2015-08-25 22:52:25 +0300

    Added trajectory storage option

--- Hermes/SQL Framework - Unofficial/NewLoader/new_loader.sql
+++ Hermes/SQL Framework - Unofficial/NewLoader/new_loader.sql
@@ -7,7 +7,7 @@
  *
  */
 
-/** @brief This function performs trajectory transformation by adding noise points (outliers) to the initial trajectory
+/** @brief This function performs dataset uploading
  *
  *	@param[in] name Name of the dataset
  *	@param[in] description Short description of the dataset
@@ -16,11 +16,13 @@
  *  @return Interger 1 to indicate succesfull completion
  *
  */
-CREATE OR REPLACE FUNCTION load_dataset(name text, description text, file text)
+CREATE OR REPLACE FUNCTION load_dataset(name text, description text, file text, trajectory_storage boolean)
 RETURNS integer
 AS $$
-
-    plpy.execute("SELECT HLoader('" + name  + "', '" + description + "')")
+    if trajectory_storage:
+        plpy.execute("SELECT hdatasetsregister('" + name + "', '" + name + "', dataset_trajectory_storage:=TRUE, dataset_segment_storage:=TRUE)")
+    else:
+        plpy.execute("SELECT HLoader('" + name  + "', '" + description + "')")
     plpy.execute("SELECT HLoaderCSV_II('" + name +  "', '" + file + "')")
     plpy.execute("SELECT HDatasetsOfflineStatistics('" + name + "')")
     plpy.execute("CREATE INDEX ON " + name  + "_seg USING gist (seg) WITH (FILLFACTOR = 100)")
